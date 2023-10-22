296b990e18b87645da48b577fca7ca0063ca0a95
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-10-22 14:14:39 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,32 @@
-mod cars_lib;
+use invidivual_project_rust_yabei::{extract, load, query};
+use std::env;
 
 fn main() {
-    // Extract
-    println!("Extracting data...");
-    match cars_lib::extract(
-        "https://raw.githubusercontent.com/yabeizeng1121/Mini_Project5/main/cars.csv",
-        "cars.csv",
-    ) {
-        Ok(file_path) => {
-            println!("Data extraction completed successfully. Saved to {}\n", file_path);
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: {} [action]", args[0]);
+        return;
+    }
 
-            // Transform and Load
-            println!("Transforming and loading data...");
-            match cars_lib::load(&file_path) {
-                Ok(_) => {
-                    println!("Data transformation and loading completed successfully.\n");
-
-                    // Query
-                    println!("Querying data...");
-                    match cars_lib::query() {
-                        Ok(_) => println!("Data querying completed successfully.\n"),
-                        Err(e) => eprintln!("An error occurred while querying data: {}", e),
-                    }
-                }
-                Err(e) => eprintln!("An error occurred while transforming and loading data: {}", e),
-            }
+    let action = &args[1];
+    match action.as_str() {
+        "extract" => {
+            let file_path = extract(
+                Some("https://raw.githubusercontent.com/yabeizeng1121/Mini_Project5/main/cars.csv"),
+                Some("cars.csv"),
+            );
+            println!("Data extraction completed successfully. Saved to {}", file_path);
         }
-        Err(e) => eprintln!("An error occurred while extracting data: {}", e),
+        "load" => {
+            let db_path = load(Some("cars.csv"));
+            println!("Data transformation and loading completed successfully. DB path: {}", db_path);
+        }
+        "query" => {
+            query();
+            println!("Data querying completed successfully.");
+        }
+        _ => {
+            println!("Invalid action. Use 'extract', 'load', or 'query'.");
+        }
     }
 }
