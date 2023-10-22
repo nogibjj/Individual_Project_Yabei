4d067aeb8649e46454eab5ce38bd6710eb9d1c8b
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-10-22 15:46:35 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -25,8 +25,20 @@ fn main() {
             query();
             println!("Data querying completed successfully.");
         }
+        "update_price" => {
+            if args.len() < 4 {
+                println!("Usage: {} update_price [brand] [new_price]", args[0]);
+                return;
+            }
+            let brand = &args[2];
+            let new_price: f64 = args[3].parse().unwrap_or(0.0);
+            match update_price(brand, new_price) {
+                Ok(_) => println!("Updated price for brand {} to {}", brand, new_price),
+                Err(e) => println!("Error updating price: {}", e),
+            }
+        }
         _ => {
-            println!("Invalid action. Use 'extract', 'load', or 'query'.");
+            println!("Invalid action. Use 'extract', 'load', 'query', or 'update_price'.");
         }
     }
 }
