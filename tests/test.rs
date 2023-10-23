9d64cf10b22d8aedcccc1769b8c996bda76ac14c
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-10-23 00:37:13 -0400

    Update test.rs

--- tests/test.rs
+++ tests/test.rs
@@ -1,15 +1,19 @@
 use invidivual_project_rust_yabei::{extract, transform_load, query};
 
 #[test]
-fn test_extract() {
+fn test_extract_and_transform_load() {
     let url = "https://raw.githubusercontent.com/yabeizeng1121/Mini_Project5/main/cars.csv";
     let file_path = "cars_test.csv";
 
+    // Extract
     extract(url, file_path).unwrap();
-
-    // Check if the file was successfully created
     assert!(std::fs::metadata(file_path).is_ok());
+
+    // Transform and Load
+    let result = transform_load(file_path);
+    assert_eq!(result.unwrap(), "CarsDB.db");
 }
+
 
 #[test]
 fn test_transform_load() {
